e5496c7a36704c49385672e26d919ea79cf8ac73
Kevin <kevin.melchert@gmail.com>
2012-11-11 02:38:52 +0100

    more test helpers

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,98 +1,89 @@
 require 'spec_helper'
 
 describe Rack::Thumb do
-  before do
-    @app = Rack::File.new(::File.dirname(__FILE__))
-    @request = Rack::MockRequest.new(Rack::Thumb.new(@app))
-    @secret_request = Rack::MockRequest.new(Rack::Thumb.new(@app, :keylength => 16, :secret => "test"))
-    @signature = Digest::SHA1.hexdigest("/media/imagick_50x100-sw.jpgtest")[0..15]
-  end
-  
   it "should render a thumbnail with width only" do
-    verify_response(@request.get("/media/imagick_50x.jpg"), "image/jpeg", [50,52])
+    verify_response(request.get("/media/imagick_50x.jpg"), "image/jpeg", [50,52])
   end
 
   it "should render a thumbnail with height only" do
-    verify_response(@request.get("/media/imagick_x50.jpg"), "image/jpeg", [48,50])
+    verify_response(request.get("/media/imagick_x50.jpg"), "image/jpeg", [48,50])
   end
-
+  
   it "should render a thumbnail with width and height (crop-resize)" do
-    verify_response(@request.get("/media/imagick_50x50.jpg"), "image/jpeg", [50,50])
+    verify_response(request.get("/media/imagick_50x50.jpg"), "image/jpeg", [50,50])
   end
-
+  
   it "should render a thumbnail with width, height and gravity (crop-resize)" do
-    verify_response(@request.get("/media/imagick_50x100-sw.jpg"), "image/jpeg", [50,100])
+    verify_response(request.get("/media/imagick_50x100-sw.jpg"), "image/jpeg", [50,100])
   end
-
+  
   it "should render a thumbnail with a signature" do
-    verify_response(@secret_request.get("/media/imagick_50x100-sw-#{@signature}.jpg"), "image/jpeg", [50,100])
+    verify_response(request(credentials).get("/media/imagick_50x100-sw-#{signature}.jpg"), "image/jpeg", [50,100])
   end
-
+  
   it "should not render a thumbnail that exceeds the original image's dimensions" do
-    verify_response(@request.get("/media/imagick_1000x1000.jpg"), "image/jpeg", [572, 591])
+    verify_response(request.get("/media/imagick_1000x1000.jpg"), "image/jpeg", [572, 591])
   end
-
+  
   it "should work with non-file source bodies" do
     app = lambda { |env| [200, {"Content-Type" => "image/jpeg"}, [::File.read(::File.dirname(__FILE__) + "/media/imagick.jpg")]] }
     request = Rack::MockRequest.new(Rack::Thumb.new(app))
     verify_response(request.get("/media/imagick_50x.jpg"), "image/jpeg", [50, 52])
   end
-
+  
   it "should return bad request if the signature is invalid" do
-    response = @secret_request.get("/media/imagick_50x100-sw-9922d04b14049f85.jpg")
+    response = request(credentials).get("/media/imagick_50x100-sw-9922d04b14049f85.jpg")
     verify_bad_response(response, 400, "Bad thumbnail parameters in /media/imagick_50x100-sw-9922d04b14049f85.jpg\n")
   end
-
+  
   it "should return bad request if the dimensions are bad" do
-    response = @request.get("/media/imagick_0x50.jpg")
+    response = request.get("/media/imagick_0x50.jpg")
     verify_bad_response(response, 400, "Bad thumbnail parameters in /media/imagick_0x50.jpg\n")
   end
-
+  
   it "should return bad request if dimensions contain leading zeroes" do
-    response = @request.get("/media/imagick_50x050.jpg")
+    response = request.get("/media/imagick_50x050.jpg")
     verify_bad_response(response, 400, "Bad thumbnail parameters in /media/imagick_50x050.jpg\n")
   end
-
+  
   it "should return the application's response if the source file is not found" do
-    response = @request.get("/media/dummy_50x50.jpg")
+    response = request.get("/media/dummy_50x50.jpg")
     verify_bad_response(response, 404, "File not found: /media/dummy.jpg\n")
   end
-
+  
   it "should return the application's response if it does not recognize render options" do
-    response = @secret_request.get("/media/imagick_50x50!.jpg")
+    response = request(credentials).get("/media/imagick_50x50!.jpg")
     verify_bad_response(response, 404, "File not found: /media/imagick_50x50!.jpg\n")
   end
-
+  
   it "should pass non-thumbnail image requests to the application" do
-    res = @secret_request.get("/media/imagick.jpg")
+    res = request(credentials).get("/media/imagick.jpg")
     res.status.must_equal 200
     res.content_type.must_equal "image/jpeg"
     res.content_length.must_equal 97374
     res.body.bytesize.must_equal 97374
   end
-
+  
   it "should not render on a HEAD request" do
-    res = @request.request("HEAD", "/media/imagick_50x50.jpg")
+    res = request.request("HEAD", "/media/imagick_50x50.jpg")
     res.status.must_equal 200
     res.content_type.must_equal "image/jpeg"
     res.content_length.must_be_nil
     res.body.bytesize.must_equal 0
   end
-
+  
   it "should preserve any extra headers provided by the downstream app" do
     app = lambda { |env| [200, {"X-Foo" => "bar", "Content-Type" => "image/jpeg"}, ::File.open(::File.dirname(__FILE__) + "/media/imagick.jpg")] }
-
     request = Rack::MockRequest.new(Rack::Thumb.new(app))
-
     res = request.request("HEAD", "/media/imagick_50x50.jpg")
     res.status.must_equal 200
     res.content_type.must_equal "image/jpeg"
     res.content_length.must_be_nil
     res.headers["X-Foo"].must_equal "bar"
   end
-
+  
   it "should forward POST/PUT/DELETE requests to the downstream app" do
-    res = @request.post("/media/imagick_50x50.jpg")
+    res = request.post("/media/imagick_50x50.jpg")
     res.status.must_equal 405
   end
   
@@ -103,10 +94,10 @@ describe Rack::Thumb do
   end
   
   it "should render a thumbnail with retina multiplier" do
-    verify_response(@request.get("/media/imagick_50x50@2x.jpg"), "image/jpeg", [100, 100])
+    verify_response(request.get("/media/imagick_50x50@2x.jpg"), "image/jpeg", [100, 100])
   end
   
   it "should render a thumbnail with a signature and retina multiplier" do
-    verify_response(@secret_request.get("/media/imagick_50x100-sw-#{@signature}@4x.jpg"), "image/jpeg", [200, 400])
+    verify_response(request(credentials).get("/media/imagick_50x100-sw-#{signature}@4x.jpg"), "image/jpeg", [200, 400])
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,15 +10,6 @@ class String
   end
 end
 
-def image_info(body)
-  t = Tempfile.new('foo.jpg').tap {|f| f.binmode; f.write(body); f.close }
-  Mapel.info(t.path)
-end
-
-def image_dimensions(response)
-  image_info(response.body)[:dimensions]
-end
-
 def verify_response(res, content_type, dim)
   res.status.must_equal 200
   res.content_type.must_equal content_type
@@ -28,4 +19,33 @@ end
 def verify_bad_response(res, status, body)
   res.status.must_equal status
   res.body.must_equal body
+end
+
+def file_app
+  @file_app ||= Rack::File.new(::File.dirname(__FILE__))
+end
+
+def request(options = {})
+  thumb_app = Rack::Thumb.new(file_app, options)
+  Rack::MockRequest.new(thumb_app)
+end
+
+def credentials
+  {:keylength => 16, :secret => "test"}
+end
+
+def tempfile(body)
+  Tempfile.new('foo.jpg').tap {|f| f.binmode; f.write(body); f.close }
+end
+
+def image_info(body)
+  Mapel.info(tempfile(body).path)
+end
+
+def image_dimensions(response)
+  image_info(response.body)[:dimensions]
+end
+
+def signature
+  Digest::SHA1.hexdigest("/media/imagick_50x100-sw.jpgtest")[0..15]
 end
